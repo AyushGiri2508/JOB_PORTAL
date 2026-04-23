c944af988ee2fbb4c06c7328fca6b76b3cdb8d57
AyushGiri2508 <ayushgiri9988777@gmail.com>
2026-04-23 21:52:24 +0530

    Add cors Origin

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','procss.env.VITE_CLIENT_URL'],
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
